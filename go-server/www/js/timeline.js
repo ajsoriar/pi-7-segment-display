c8562546acfa64c206e8febb8f3ab67900836146
asr <ajsoriar@gmail.com>
2018-07-10 00:06:42 +0200

    some changes

--- go-server/www/js/timeline.js
+++ go-server/www/js/timeline.js
@@ -6,6 +6,8 @@ AS.attachAnimation({
     repeatForever: true,
     func: function( _animation ) {
         console.log("animation: step!");
+        ajsrClock.update(1000);
+        timeCanvas.refresh();
     },
     onStart: function() { 
         console.log("animation: onStart!");
@@ -14,3 +16,100 @@ AS.attachAnimation({
         console.log("animation: whenFinish!");
     }
 })
+
+var ajsrClock = {
+    hh: null,
+    mm: null,
+    ss: null,
+    lastTimestamp: null,
+    onGoingUpdate: false,
+    el: null,
+    id: null,
+    init: function(miliseconds){
+        console.log("[ajsrClock] init()");
+        if (miliseconds === null) { 
+            this.lastTimestamp = Date.now(); 
+        } else { 
+            this.lastTimestamp = miliseconds; 
+        }
+
+        // create html
+        if ( this.el === null){
+            this.el = "ajsrClock-"+ Date.now();
+            var str = '';
+            str += '<div id="'+ this.el +'" class="ajsrClock"></div>';
+        }
+
+        if ( this.onGoingUpdate === false) this.update();
+    },
+    update: function(miliseconds){
+        this.onGoingUpdate = true;
+        console.log("[ajsrClock] update()");
+
+        if ( this.lastTimestamp === null ) this.init();
+
+        if (miliseconds === null) { 
+            this.paint( miliseconds );
+        } else { 
+            this.paint( this.lastTimestamp );  
+        }
+
+        this.onGoingUpdate = false;
+    },
+    reset: function(){
+        console.log("[ajsrClock] reset()");
+
+    },
+    paint: function(miliseconds){
+        if (miliseconds === null) { 
+            //this.paint( miliseconds );
+
+        } else { 
+            //this.paint( this.lastTimestamp );
+
+        }
+    },
+    goForward: function(miliseconds){
+
+    },
+    goBackwards: function(miliseconds){
+
+    }
+}
+
+var timeCanvas = {
+    // hh: null,
+    // mm: null,
+    // ss: null,
+    // lastTimestamp: null,
+    currentTimestamp: 0,
+    onGoingUpdate: false,
+    el: null,
+    // id: null,
+    init: function(){
+        console.log("[timeCanvas] init()");
+
+        // create html
+        if ( this.el === null){
+            this.el = "timeCanvas-"+ Date.now();
+            var str = '';
+            str += '<div id="'+ this.el +'" class="timeCanvas"></div>';
+        }
+
+        //if ( this.onGoingUpdate === false) this.update();
+    },
+    update: function(){
+        // this.onGoingUpdate = true;
+        // console.log("[timeCanvas] update()");
+
+
+        // this.onGoingUpdate = false;
+    },
+    reset: function(){
+        console.log("[timeCanvas] reset()");
+
+    },
+    refresh: function( time ){
+        console.log("[timeCanvas] refresh()");
+    }
+}
